59f3ea8f8c6f0ad151fd17dbc56a8f440b3afa07
Hendrick Osseux <osseux.hendrick@gmail.com>
2021-11-06 12:18:03 +0100

    Update & Delete Contact

--- src/main/java/com/example/PayMyBuddy/controller/ContactController.java
+++ src/main/java/com/example/PayMyBuddy/controller/ContactController.java
@@ -1,5 +1,7 @@
 package com.example.PayMyBuddy.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
@@ -7,19 +9,16 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.example.PayMyBuddy.domain.ContactEntity;
 import com.example.PayMyBuddy.domain.UserEntity;
 import com.example.PayMyBuddy.persistence.ContactDao;
-import com.example.PayMyBuddy.persistence.PersistenceException;
 import com.example.PayMyBuddy.persistence.UserDao;
 import com.example.PayMyBuddy.security.LoginUtils;
 import com.example.PayMyBuddy.security.Roles;
@@ -55,32 +54,63 @@ public class ContactController {
 		return findAllContacts();
 	}
 
+//	/* Mettre à jour un contact */
+//	@PutMapping
+//	@ResponseStatus(HttpStatus.OK)
+//	public ContactEntity updateInfoContact(@RequestBody ContactEntity contactEntity) {
+//		try {
+//			return contactDao.updateContact(contactEntity);
+//		} catch (PersistenceException e) {
+//			throw new ResponseStatusException(e.getStatus(), e.getMessage());
+//		}
+//	}
+
 	/* Mettre à jour un contact */
 	@PutMapping
 	@ResponseStatus(HttpStatus.OK)
-	public ContactEntity updateInfoContact(@RequestBody ContactEntity contactEntity) {
-		try {
-			return contactDao.updateContact(contactEntity);
-		} catch (PersistenceException e) {
-			throw new ResponseStatusException(e.getStatus(), e.getMessage());
+	public Iterable<ContactEntity> updateInfoContact(@RequestBody ContactEntity contactEntity) {
+		User user = LoginUtils.getLoggedUser();
+		UserEntity userEntity = userDao.findByEmail(user.getUsername());
+		ContactEntity contact = getContactFromId(userEntity, contactEntity.getId());
+		if (contactEntity.getUsername() != null) {
+			contact.setUsername(contactEntity.getUsername());
 		}
-	}
-
-	/* Supprimer un contact */
-	@DeleteMapping(value = "existing/{id}")
-	@ResponseStatus(HttpStatus.OK)
-	public Iterable<ContactEntity> deleteExistingContact(@PathVariable("id") Integer id) {
-		contactDao.deleteContact(id);
+		if (contactEntity.getDescription() != null) {
+			contact.setDescription(contactEntity.getDescription());
+		}
+		if (contactEntity.getSolde() != null) {
+			contact.setSolde(contactEntity.getSolde());
+		}
+		userDao.save(userEntity);
 		return findAllContacts();
 	}
 
+	private ContactEntity getContactFromId(UserEntity userEntity, Integer id) {
+		Set<ContactEntity> contacts = userEntity.getContacts();
+		for (ContactEntity contactEntity : contacts) {
+			if (contactEntity.getId().equals(id)) {
+				return contactEntity;
+			}
+		}
+		return null;
+	}
+
 	/* Supprimer un contact */
-	@DeleteMapping(value = "fromlist/{id}")
+//	@DeleteMapping(value = "existing/{id}")
+//	@ResponseStatus(HttpStatus.OK)
+//	public Iterable<ContactEntity> deleteExistingContact(@PathVariable("id") Integer id) {
+//		contactDao.deleteContact(id);
+//		return findAllContacts();
+//	}
+
+	/* Supprimer un contact */
+	@DeleteMapping
 	@ResponseStatus(HttpStatus.OK)
 	public Iterable<ContactEntity> deleteContactFromList(@RequestBody ContactEntity contactEntity) {
 		User user = LoginUtils.getLoggedUser();
 		UserEntity userEntity = userDao.findByEmail(user.getUsername());
-		userEntity.getContacts().remove(contactEntity);
+		ContactEntity contact = getContactFromId(userEntity, contactEntity.getId());
+		userEntity.getContacts().remove(contact);
 		userDao.save(userEntity);
 		return findAllContacts();
 	}
